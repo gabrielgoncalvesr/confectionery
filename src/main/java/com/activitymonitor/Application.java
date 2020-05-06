644f5943979a66c6b2e7196fa4a73aca696706db
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-05-06 00:19:48 -0300

    changing to basic structure

--- src/main/java/com/activitymonitor/Application.java
+++ src/main/java/com/activitymonitor/Application.java
@@ -1,17 +1,12 @@
 package com.activitymonitor;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.activitymonitor.activity.service.ActivityService;
 
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	private ActivityService activityService;
-	
-//	public static void main(String[] args) {
-//		SpringApplication.run(Application.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
